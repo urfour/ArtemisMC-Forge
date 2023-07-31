b27201a7301fcbb685d4fa6375975f528e2223df
Nassime Mountasir <nassimem@live.fr>
2023-07-31 11:39:08 +0200

    added hotbar slot and logo

--- src/main/java/com/urfour/artemis/server/Server.java
+++ src/main/java/com/urfour/artemis/server/Server.java
@@ -37,7 +37,6 @@ public class Server implements Runnable {
 
     public void run() {
         try {
-            System.out.println(gson.toJson(infos));
             infos.update();
             HttpClient httpClient = HttpClientBuilder.create().build();
             HttpPost request = new HttpPost(IP + "plugins/25dacd2d-9275-4d94-bc12-8761dedf0f1d/Minecraft");
